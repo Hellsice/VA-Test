13db5ffd6f91941cee8fa9c1135b580daa0e9c8a
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 11:26:34 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -36,7 +36,7 @@ df = countries_geojson.merge(GDP, left_on='ISO_A3', right_on='Country Code', how
 rampen_df = pd.read_csv('rampen_df.csv')
 
 
-
+pages = 'Comparison disasters'
 
 if pages== 'Map' or pages == 'Economic change' or pages == 'Comparison disasters':
     with st.form(key='my_form'):
@@ -44,6 +44,37 @@ if pages== 'Map' or pages == 'Economic change' or pages == 'Comparison disasters
         Total_affected_mult = st.slider('Set the total affected multiplier',min_value=0.0, value=0.3 ,max_value=1.0, step=0.01)
         Intensity_threshold = st.number_input('Set the intensity threshold (default: 0.00001)', min_value=0.0, value=0.00001, max_value=1.0, step=0.00001)
         jaar = st.slider('Select year',min_value=1961, value=2018 ,max_value=2018)
+        
+        round_mult = 100000
+        rampen_df['Intensity'] = 0
+        for i in range(len(rampen_df)):
+            a = Population[Population['Country Code']==rampen_df['ISO'][i]]
+            if len(a) == 1:
+                rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])
+
+
+        rampen_df = rampen_df[rampen_df['Intensity'] >= Intensity_threshold].reset_index(drop=True)
+        rampen_df = rampen_df[['Year', 'ISO', 'Country', 'Disaster Group', 'Disaster Subgroup', 'Disaster Type',
+               'Disaster Subtype', 'Total Deaths', 'Total Affected new', 'Intensity', 'Jaar 0', 'Jaar 1', 'Jaar 2', 'Jaar 3']]
+
+
+
+
+        quantiles = rampen_df.groupby(['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'])['Intensity'].quantile([0.25, 0.75]).reset_index()
+        quantiles = quantiles.pivot(index=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], 
+                                    columns = 'level_4', values='Intensity').reset_index()
+        test = rampen_df.merge(quantiles, left_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], 
+                               right_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], how='left')
+        test.columns = ['Year','ISO','Country','Disaster Group','Disaster Subgroup','Disaster Type',
+                        'Disaster Subtype','Total Deaths','Total Affected new','Intensity','Jaar 0','Jaar 1','Jaar 2','Jaar 3',"0.25",'0.75']
+        test['Category']=0
+        for index, row in test.iterrows():
+            test.iloc[index, test.columns.get_loc('Category')] = 3 if row['Intensity']>row['0.75'] else (1 if row['Intensity']<row['0.25'] else 2)
+        rampen_df['Category'] = test['Category']
+
+        rampen_df = rampen_df.sort_values(by='Year').reset_index(drop=True)
+        
+        
         if pages == 'Map':
             Soort_data = ''
             Soort_data = st.selectbox('Select data type', ['Intensity', 'Affected'])
@@ -65,37 +96,6 @@ if pages== 'Map' or pages == 'Economic change' or pages == 'Comparison disasters
             category_dict = dict(zip(categories, category))
             category_box = st.selectbox('Kies een Ramp categorie', categories)
   
-
-    round_mult = 100000
-    rampen_df['Intensity'] = 0
-    for i in range(len(rampen_df)):
-        a = Population[Population['Country Code']==rampen_df['ISO'][i]]
-        if len(a) == 1:
-            rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])
-
-
-    rampen_df = rampen_df[rampen_df['Intensity'] >= Intensity_threshold].reset_index(drop=True)
-    rampen_df = rampen_df[['Year', 'ISO', 'Country', 'Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-           'Disaster Subtype', 'Total Deaths', 'Total Affected new', 'Intensity', 'Jaar 0', 'Jaar 1', 'Jaar 2', 'Jaar 3']]
-
-
-
-
-    quantiles = rampen_df.groupby(['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'])['Intensity'].quantile([0.25, 0.75]).reset_index()
-    quantiles = quantiles.pivot(index=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], 
-                                columns = 'level_4', values='Intensity').reset_index()
-    test = rampen_df.merge(quantiles, left_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], 
-                           right_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type','Disaster Subtype'], how='left')
-    test.columns = ['Year','ISO','Country','Disaster Group','Disaster Subgroup','Disaster Type',
-                    'Disaster Subtype','Total Deaths','Total Affected new','Intensity','Jaar 0','Jaar 1','Jaar 2','Jaar 3',"0.25",'0.75']
-    test['Category']=0
-    for index, row in test.iterrows():
-        test.iloc[index, test.columns.get_loc('Category')] = 3 if row['Intensity']>row['0.75'] else (1 if row['Intensity']<row['0.25'] else 2)
-    rampen_df['Category'] = test['Category']
-
-    rampen_df = rampen_df.sort_values(by='Year').reset_index(drop=True)
-    
-    
 
                 
 
