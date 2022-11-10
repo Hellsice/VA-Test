33386d3f3abc716872c91d132ba325909064c0af
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 12:26:28 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -143,7 +143,7 @@ if pages == 'Map':
         df_adjusted = rampen_df_affected
 
     map = px.choropleth_mapbox(df_adjusted, geojson=df_adjusted.geometry, locations=df_adjusted.index, color=jaar, mapbox_style="open-street-map",
-                          hover_name=df_adjusted.Country, zoom=1)    
+                          hover_name=df_adjusted.Country, zoom=1, height=800)    
     st.plotly_chart(map, use_container_width=True)
 
 
