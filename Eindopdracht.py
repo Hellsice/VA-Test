6292383f576b1cc5d8dbadc9314b0755bdfe57e4
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 14:34:57 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -262,6 +262,10 @@ if pages == 'Economic change':
     scatter_df = rampen_df[rampen_df['ISO']==land_code]
     scatter_df = scatter_df[(scatter_df['Year']>=grafiek_min_jaar) & (scatter_df['Year']<=grafiek_max_jaar)]
     scatter_graph = px.scatter(x=scatter_df['Year'], y=scatter_df['Intensity'])
+    scatter_graph.update_traces(marker=dict(size=12,
+                              line=dict(width=2,
+                                        color='Red')),
+                  selector=dict(mode='markers'))
     with col6:
         st.plotly_chart(scatter_graph)
 
