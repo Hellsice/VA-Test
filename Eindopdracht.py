0578ca09d816cd9ec2a6640f1a693ed5cc8c11fc
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 10:47:54 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -49,47 +49,25 @@ if pages== 'Map' or pages == 'Economic change' or pages == 'Comparison disasters
             Soort_data = st.selectbox('Select data type', ['Intensity', 'Affected'])
             Soort_data_dict = {'Intensity':'Intensity',
                                'Affected':'Affected'}
-            
-        
-        
-
-    round_mult = 100000
-    rampen_df['Intensity'] = 0
-
-    for i in range(len(rampen_df)):
-        a = Population[Population['Country Code']==rampen_df['ISO'][i]]
-        if len(a) == 1:
-            rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])
-        
-        
-    rampen_df = rampen_df[rampen_df['Intensity'] >= Intensity_threshold].reset_index(drop=True)
-    rampen_df = rampen_df[['Year', 'ISO', 'Country', 'Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-       'Disaster Subtype', 'Total Deaths', 'Total Affected new', 'Intensity', 'Jaar 0', 'Jaar 1', 'Jaar 2', 'Jaar 3']]
-
-
-
-
-    quantiles = rampen_df.groupby(['Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-        'Disaster Subtype'])['Intensity'].quantile([0.25, 0.75]).reset_index()
-    quantiles = quantiles.pivot(index=['Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-       'Disaster Subtype'], columns = 'level_4', values='Intensity').reset_index()
-    test = rampen_df.merge(quantiles, left_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-        'Disaster Subtype'], right_on=['Disaster Group', 'Disaster Subgroup', 'Disaster Type',
-        'Disaster Subtype'], how='left')
-    test.columns = ['Year','ISO','Country','Disaster Group','Disaster Subgroup','Disaster Type',
-                    'Disaster Subtype','Total Deaths','Total Affected new',
-                    'Intensity','Jaar 0','Jaar 1','Jaar 2','Jaar 3',"0.25",'0.75']
-    test['Category']=0
-    for index, row in test.iterrows():
-        test.iloc[index, test.columns.get_loc('Category')] = 3 if row['Intensity']>row['0.75'] else (1 if row['Intensity']<row['0.25'] else 2)
-    rampen_df['Category'] = test['Category']
-
-    rampen_df = rampen_df.sort_values(by='Year').reset_index(drop=True)
-
-
-    
-
-
+        if pages == 'Economic change':
+                landen = rampen_df[['Country', 'ISO']]
+                landen_naam = rampen_df['Country'].unique()
+                landen_dict = landen.set_index('Country').to_dict()
+                landen_dict = landen_dict['ISO']
+                landen_box = st.selectbox('Choose a country', landen_naam)
+                land_code= landen_dict[landen_box]
+        if pages == 'Comparison disasters':
+                types = list(rampen_df['Disaster Subtype'].unique())  
+                type_names = list(rampen_df['Disaster Subtype'].unique())
+                type_dict = dict(zip(types, type_names))
+                type_box=st.selectbox('Kies een subtype', types)
+                categories = ['Categorie 1', 'Categorie 2', 'Categorie 3']
+                category= ['Category 1', 'Category 2', 'Category 3']
+                category_dict = dict(zip(categories, category))
+                category_box = st.selectbox('Kies een Ramp categorie', categories)
+                
+                
+                
 
 if pages == 'Map':
     if Soort_data_dict[Soort_data]=='Intensity':
@@ -115,22 +93,12 @@ if pages == 'Map':
         rampen_df_affected = countries_geojson.merge(rampen_df_affected, left_on='ISO_A3', right_on='ISO', how='right')
         df_adjusted = rampen_df_affected
 
-
     map = px.choropleth_mapbox(df_adjusted, geojson=df_adjusted.geometry, locations=df_adjusted.index, color=jaar, mapbox_style="open-street-map",
                           hover_name=df_adjusted.Country, zoom=1)    
-
-
     st.plotly_chart(map)
 
 
 if pages == 'Economic change':
-    landen = rampen_df[['Country', 'ISO']]
-    landen_naam = rampen_df['Country'].unique()
-    landen_dict = landen.set_index('Country').to_dict()
-    landen_dict = landen_dict['ISO']
-    landen_box = st.selectbox('Choose a country', landen_naam)
-    land_code= landen_dict[landen_box]
-
     col5, col6 = st.columns([1,1])
     land = landen_box
     check = rampen_df[rampen_df['Country']==landen_box]
@@ -168,30 +136,16 @@ if pages == 'Economic change':
     with col5:
         GDP_land
 
-
-
-
     GDP_fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-
     GDP_fig.add_trace(
         go.Line(x=GDP_grafiek['Year'].to_list(), y=GDP_grafiek[land_code].to_list(), name=landen_box),
-        secondary_y=False,
-    )
-
+        secondary_y=False)
     GDP_fig.add_trace(
         go.Line(x=GDP_grafiek['Year'].to_list(), y=GDP_grafiek['WLD'].to_list(), name="World"),
-        secondary_y=True,
-    )
-
-
+        secondary_y=True)
     GDP_fig.update_layout(
-        title_text="<b>GDP comparising of world vs. " + landen_box +'</b>'
-    )
-
-
+        title_text="<b>GDP comparising of world vs. " + landen_box +'</b>')
     GDP_fig.update_xaxes(title_text="<b>2 years before and 5 years after chosen year</b>")
-    
     GDP_fig.update_yaxes(title_text='<b>GDP ' + landen_box + '</b>', secondary_y=False)
     GDP_fig.update_yaxes(title_text='<b>GDP world</b>', secondary_y=True)
     with col6:
@@ -200,19 +154,10 @@ if pages == 'Economic change':
 
 
 
-if pages == 'Comparison disasters':
-    types = list(rampen_df['Disaster Subtype'].unique())
-       
-    type_names = list(rampen_df['Disaster Subtype'].unique())
-    type_dict = dict(zip(types, type_names))
-    type_box=st.selectbox('Kies een subtype', types)
-    
+if pages == 'Comparison disasters': 
     col3, col4 = st.columns([1,1])
-    
     data_subtypes = rampen_df[rampen_df['Disaster Subtype']==type_box].reset_index(drop=True)
-    
     Fig_subtypes_jaar0 = px.box(data_subtypes, x='Category', y='Jaar 0', color='Category')
-    
     Fig_subtypes_jaar0.update_layout(title='<b>'+data_subtypes['Disaster Subtype'][0] + ': Jaar 0</b>')
     Fig_subtypes_jaar0.update_yaxes(title = '<b>GDP change compared to world</b>')
     Fig_subtypes_jaar0.update_xaxes(title = '<b>Category</b>')
@@ -222,7 +167,6 @@ if pages == 'Comparison disasters':
     
     
     Fig_subtypes_jaar1 = px.box(data_subtypes, x='Category', y='Jaar 1', color='Category')
-    
     Fig_subtypes_jaar1.update_layout(title='<b>'+data_subtypes['Disaster Subtype'][0] + ': Jaar 1</b>')
     Fig_subtypes_jaar1.update_yaxes(title = '<b>GDP change compared to world</b>')
     Fig_subtypes_jaar1.update_xaxes(title = '<b>Category</b>')
@@ -232,7 +176,6 @@ if pages == 'Comparison disasters':
     
     
     Fig_subtypes_jaar2 = px.box(data_subtypes, x='Category', y='Jaar 2', color='Category')
-    
     Fig_subtypes_jaar2.update_layout(title='<b>'+data_subtypes['Disaster Subtype'][0] + ': Jaar 2</b>')
     Fig_subtypes_jaar2.update_yaxes(title = '<b>GDP change compared to world</b>')
     Fig_subtypes_jaar2.update_xaxes(title = '<b>Category</b>')
@@ -242,7 +185,6 @@ if pages == 'Comparison disasters':
     
     
     Fig_subtypes_jaar3 = px.box(data_subtypes, x='Category', y='Jaar 3', color='Category')
-    
     Fig_subtypes_jaar3.update_layout(title='<b>'+data_subtypes['Disaster Subtype'][0] + ': Jaar 3</b>')
     Fig_subtypes_jaar3.update_yaxes(title = '<b>GDP change compared to world</b>')
     Fig_subtypes_jaar3.update_xaxes(title = '<b>Category</b>')
@@ -251,13 +193,8 @@ if pages == 'Comparison disasters':
         st.plotly_chart(Fig_subtypes_jaar3)
     
     
-    
-    
-    
-    categories = ['Categorie 1', 'Categorie 2', 'Categorie 3']
-    category= ['Category 1', 'Category 2', 'Category 3']
-    category_dict = dict(zip(categories, category))
-    category_box = st.selectbox('Kies een Ramp categorie', categories)
+
+
     
     if category_dict[category_box] == 'Category 1':
         Category_data = rampen_df[(rampen_df['Category']==1)]
