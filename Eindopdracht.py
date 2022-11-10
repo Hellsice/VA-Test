4acc834afa11276f0524f0f3c57990f8d40f5c70
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 21:54:35 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -310,7 +310,8 @@ if pages == 'Economic change':
         title_text="<b>GDP comparison of world vs. " + landen_box +'</b>', title_x=0.3)
     GDP_fig.update_xaxes(title_text="<b>2 years before and 5 years after chosen year</b>")
     GDP_fig.update_yaxes(title_text='<b>GDP ' + landen_box + '</b>', secondary_y=False)
-    GDP_fig.update_yaxes(title_text='<b>GDP world</b>', secondary_y=True)
+    GDP_fig.update_yaxes(title_text='<b>GDP world</b>', secondary_y=True, font=dict(size=18))
+    
     with col7:
         st.plotly_chart(GDP_fig)
     
