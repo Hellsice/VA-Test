46504e48af1e9b69140fb8dd5739afecde64a940
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-11-10 19:14:39 +0100

    Update Eindopdracht.py

--- Eindopdracht.py
+++ Eindopdracht.py
@@ -78,16 +78,11 @@ rampen_df_controle2 = rampen_df.groupby(['ISO', 'Country', 'Year', 'Disaster Gro
     st.markdown('')
     st.markdown('')
     st.markdown('Bepaling van intensiteit')
-    st.code("rampen_df['Intensity'] = 0\n\
-for i in range(len(rampen_df)):\n\
-    a = Population[Population['Country Code']==rampen_df['ISO'][i]]\n\
-    if len(a) == 1:\n\
-        rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])", language='python')
     st.code('''rampen_df['Intensity'] = 0
-        for i in range(len(rampen_df)):
-            a = Population[Population['Country Code']==rampen_df['ISO'][i]]
-            if len(a) == 1:
-                rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])''', language='python')
+    for i in range(len(rampen_df)):
+        a = Population[Population['Country Code']==rampen_df['ISO'][i]]
+        if len(a) == 1:
+            rampen_df['Intensity'][i] = (rampen_df['Total Deaths'][i]+Total_affected_mult*rampen_df['Total Affected new'][i])/(a[str(rampen_df['Year'][i])].values[0])''', language='python')
 
 
 
